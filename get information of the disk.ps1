e441a27be4d6984196966b3bdf0656c4a82ec673
Krip4us <44360877+Krip4us@users.noreply.github.com>
2018-11-25 11:48:10 +0100

    Update get information of the disk.ps1

--- get information of the disk.ps1
+++ get information of the disk.ps1
@@ -1,8 +1,5 @@
-'''powershell
 $my_disk = %{
 get-wmiobject win32_volume -computername . | select name, @{expression={$_.capacity/1GB}}, @{expression={$_.freespace/1GB}}, @{name=”% Free”;expression={$_.freespace/$_.capacity*100}}
 }
 $my_disk
 $my_disk | Out-File resltados.txt -Append
-
-'''
