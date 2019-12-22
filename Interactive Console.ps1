1dfb2a5b1f6e024dd549e84671ab1af0cc6f1e58
Krip4us <44360877+Krip4us@users.noreply.github.com>
2019-12-22 13:12:13 +0100

    Update Interactive Console.ps1

--- Interactive Console.ps1
+++ Interactive Console.ps1
@@ -6,22 +6,26 @@ else {
     New-Item -Path $Profile.CurrentUserAllHosts -ItemType File -Force
 }
 '
+
 Clear-Host;Set-Location $env:USERPROFILE
-$date=(Get-Date).ToString("dd.MM.yyyy/HH:mm:ss");
+function date{
+    (Get-Date).ToString("dd.MM.yyyy/HH:mm:ss"); 
+}
+$date = date
 Write-Host "
-                                            #######################################
-                                                        BUENOS DIAS
-                                            inicio de sesion:> $date
-                                                        help:commands
-                                            #######################################
-" -ForegroundColor Blue;
-$date>>"LastAccessTime"
+#######################################################
+            inicio de sesion ==>  $date      
+            HELP COMMANDS    ==>  commands
+#######################################################
+" -ForegroundColor GREEN ;
+
+date >>"LastAccessTime"
 function tras{
     Remove-Item "LastAccessTime" -Force
     Write-Host "file lastaccesstime deleted!!!" -ForegroundColor Red
 }
 function renew {
-    $date>>"LastAccessTime"
+    date >>"LastAccessTime"
     Write-Host "file lastaccesstime renew!!!" -ForegroundColor Red
 }
 function view{
@@ -107,87 +111,103 @@ function findex{
 }
 function check{
     function checkerPS1{
-        cd $env:SystemDrive\
-$random = Get-Random -Maximum 100 -Minimum 1 
-$random
-ni -ItemType Directory -Name "$env:COMPUTERNAME;INFO;$random" -ErrorAction SilentlyContinue ; cd "$env:COMPUTERNAME;INFO;$random"
-
-function Get_Date{
-    $date = (Get-Date).ToString("yyyy.MM.dd")
-    $dateSortable = (Get-Date -Format u)
-    $datesort =  (Get-Date -Format T)
-    $Sorter = (Get-Date -Format d)
-
-$date ;$dateSortable ;$datesort ; $Sorter
-@{
-    ToString = $date
-    Format = $dateSortable
-    Sort = $datesort
-    SortDate = $Sorter
-}
-
-}
-Get_Date | out-file CHECKER.log
-
-
-
-##############################################################################
-function Check{
-    Get-WmiObject -Class win32_Desktop | Out-File win32_Desktop.sys
-    systeminfo.exe | Out-File systeminfo.sys
-    Get-PSDrive | Out-File PSDrive.sys
-    Get-Service | Format-Custom | Out-File Service.sys
-    Get-Process | Out-File Process.sys
-    Get-PnpDevice | sort-object -property status -descending | Out-File PnpDevice.sys
-    Get-Disk | Out-File Disk.sys
-    Get-NetTCPConnection | Select * | Format-List | Out-File NetTCPConnection.sys
-    $wc = new-object System.Net.Webclient
-    $wc.DownloadString("http://icanhazip.com/") | Out-File IPv6.sys
-    nslookup myip.opendns.com resolver1.opendns.com | Out-File NsLookup.sys
-    ipconfig.exe /displaydns
-    Get-NetAdapterBinding -Name 'USB-ETH1','Ethernet 2' |Sort-Object -Property Enabled -CaseSensitive | Out-File 'Get-NetAdapterBinding.sys'
-    Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\USBSTOR\*\*\' | Where-Object { $_.FriendlyName } | Select-Object FriendlyName | Out-File 'USBSTOR.sys'
-    Get-WmiObject -Class Win32_NetworkAdapter | Where-Object { $_.Speed -ne $null -and $_.MACAddress -ne $null } | Format-List | Out-File Win32_NetworkAdapter.sys
-}
-
-function action{
-    "-------------------------------------------"
-    ([System.Security.Principal.WindowsIdentity]::GetCurrent()).name | Out-File CurrentUser.sys
-    "-------------------------------------------"
-    (Get-WmiObject Win32_OperatingSystem | select PSComputerName,status,name,SystemDevice,Debug,Distributed,Primary | Format-List)| Out-File Win32_OperatingSystem.sys
-        "-------------------------------------------"
-    Get-WmiObject Win32_UserProfile | %{$_.LocalPath, $_.ConvertToDateTime($_.LastUseTime)}| Out-File Win32_UserProfile.sys
-        "-------------------------------------------" 
-    Get-WmiObject win32_process | Sort-Object Processid | Select-Object Processid,Name,CommandLine| Out-File win32_process.sys
-        "-------------------------------------------"
-    Get-WmiObject -Class Win32_Service | Select-Object Name,@{Name="ProcessID";Expression={$_.ProcessID;(Get-Process -Id $_.ProcessID).name}}| Out-File Win32_Service.sys
-        "-------------------------------------------"
-    Get-WmiObject Win32_PhysicalMemory | Select-Object ConfiguredClockSpeed| Out-File ConfiguredClockSpeed.sys
-        "-------------------------------------------"
-    Get-WmiObject Win32_PhysicalMemory | Select-Object MinVoltage| Out-File MinVoltage.sys
-        "-------------------------------------------" 
-    Get-WmiObject -Query "select * from win32_service where name='WinRM'" -ComputerName $env:COMPUTERNAME | Format-List -Property PSComputerName, Name, ExitCode, Name, ProcessID, StartMode, State, Status| Out-File WinRM.sys
-        "-------------------------------------------"
-
-    }
-    
-#################
-Check ;
-action  
-#################
-Set-Location ..;
-Compress-Archive -Path  "$env:COMPUTERNAME;INFO;$random" -DestinationPath  "$env:COMPUTERNAME;INFO;$random.zip";
-if ((Test-Path "$env:COMPUTERNAME;INFO;$random.zip")-eq 'True') {
-    rmdir "$env:COMPUTERNAME;INFO;$random" -Recurse -Force;
-'________________________________________________________________________________________________'
-Write-Host "CHECKER FINISH COMPLETE!!" -ForegroundColor Green
-'________________________________________________________________________________________________'
-exit
-}
-else {
-    Write-Host 'checker complete but not compressed' -ForegroundColor Red
-    Start-Sleep -Seconds 6;exit
-}
+        function info {
+            [ordered]@{
+                "AUTOR" = "https://github.com/Krip4us"
+                "NOMBRE DEL PROGRAMA" = "CHECKER.ps1"
+                "version" = "2.0.5.1"
+                "PERTENENCIA" = "files.programing.codes.lion.powershell"
+                "DOMINIO" = "asusyred.local.es"
+                "ULTIMA MODIFICACION" = "23/11/19"
+                "PLAZO GASTADO" =  "5meses (12/06/19)"
+            }   |Out-GridView 
+        }
+        cd \\Asus-dc001\nuxeo\FileHistory
+        $random = Get-Random -Maximum 100 -Minimum 1 
+        $random
+        ni -ItemType Directory -Name "$env:COMPUTERNAME;INFO;$random" -ErrorAction SilentlyContinue ; cd "$env:COMPUTERNAME;INFO;$random"
+        
+        function Get_Date{
+            $date = (Get-Date).ToString("yyyy.MM.dd")
+            $dateSortable = (Get-Date -Format u)
+            $datesort =  (Get-Date -Format T)
+            $Sorter = (Get-Date -Format d)
+        
+        $date ;$dateSortable ;$datesort ; $Sorter
+        @{
+            ToString = $date
+            Format = $dateSortable
+            Sort = $datesort
+            SortDate = $Sorter
+        }
+        
+        }
+        Get_Date | out-file CHECKER.log
+        
+        function LastLog{
+            $Sorter+"-"+$datesort+"
+            File Number=>"+"$random ;;>\\Asus-dc001\public-downloads\powershell;;>NUXEO(G:\FileHistory) " | Out-File "\\Asus-dc001\public-downloads\LastCheck.log"
+        }
+        LastLog
+        
+        ##############################################################################
+        function Check{
+            Get-WmiObject -Class win32_Desktop | Out-File win32_Desktop.sys
+            systeminfo.exe | Out-File systeminfo.sys
+            Get-PSDrive | Out-File PSDrive.sys
+            Get-Service | Format-Custom | Out-File Service.sys
+            Get-Process | Out-File Process.sys
+            Get-PnpDevice | sort-object -property status -descending | Out-File PnpDevice.sys
+            Get-Disk | Out-File Disk.sys
+            Get-NetTCPConnection | Select * | Format-List | Out-File NetTCPConnection.sys
+            $wc = new-object System.Net.Webclient
+            $wc.DownloadString("http://icanhazip.com/") | Out-File IPv6.sys
+            nslookup myip.opendns.com resolver1.opendns.com | Out-File NsLookup.sys
+            ipconfig.exe /displaydns
+            Get-NetAdapterBinding -Name "USB-ETH1","Ethernet 2" |Sort-Object -Property Enabled -CaseSensitive | Out-File "Get-NetAdapterBinding.sys"
+            Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\USBSTOR\*\*\" | Where-Object { $_.FriendlyName } | Select-Object FriendlyName | Out-File "USBSTOR.sys"
+            Get-WmiObject -Class Win32_NetworkAdapter | Where-Object { $_.Speed -ne $null -and $_.MACAddress -ne $null } | Format-List | Out-File Win32_NetworkAdapter.sys
+        }
+        
+        function action{
+            "-------------------------------------------"
+            ([System.Security.Principal.WindowsIdentity]::GetCurrent()).name | Out-File CurrentUser.sys
+            "-------------------------------------------"
+            (Get-WmiObject Win32_OperatingSystem | select PSComputerName,status,name,SystemDevice,Debug,Distributed,Primary | Format-List)| Out-File Win32_OperatingSystem.sys
+                "-------------------------------------------"
+            Get-WmiObject Win32_UserProfile | %{$_.LocalPath, $_.ConvertToDateTime($_.LastUseTime)}| Out-File Win32_UserProfile.sys
+                "-------------------------------------------" 
+            Get-WmiObject win32_process | Sort-Object Processid | Select-Object Processid,Name,CommandLine| Out-File win32_process.sys
+                "-------------------------------------------"
+            Get-WmiObject -Class Win32_Service | Select-Object Name,@{Name="ProcessID";Expression={$_.ProcessID;(Get-Process -Id $_.ProcessID).name}}| Out-File Win32_Service.sys
+                "-------------------------------------------"
+            Get-WmiObject Win32_PhysicalMemory | Select-Object ConfiguredClockSpeed| Out-File ConfiguredClockSpeed.sys
+                "-------------------------------------------"
+            Get-WmiObject Win32_PhysicalMemory | Select-Object MinVoltage| Out-File MinVoltage.sys
+                "-------------------------------------------" 
+            Get-WmiObject -class win32_service -ComputerName ASUS-DC001 | where-object name -match "WinRM" | Format-List -Property PSComputerName, Name, ExitCode, Name, ProcessID, StartMode, State, Status| Out-File WinRM.sys
+                "-------------------------------------------"
+        
+            }
+            
+        #################
+        Check ;
+        action  
+        #################
+        Set-Location ..;
+        Compress-Archive -Path  "$env:COMPUTERNAME;INFO;$random" -DestinationPath  "$env:COMPUTERNAME;INFO;$random.zip";
+        if ((Test-Path "$env:COMPUTERNAME;INFO;$random.zip")-eq "True") {
+            rmdir "$env:COMPUTERNAME;INFO;$random" -Recurse -Force;
+        "________________________________________________________________________________________________"
+        Write-Host "CHECKER FINISH COMPLETE!!" -ForegroundColor Green
+        "________________________________________________________________________________________________"
+        exit
+        }
+        else {
+            Write-Host "checker complete but not compressed" -ForegroundColor Red
+            Start-Sleep -Seconds 6;exit
+        }
+        
     }
     Read-Host "start checker.ps1?(y/n)"|ForEach-Object{
         if ($_ -eq "y") {
@@ -275,7 +295,4 @@ function commands{
         "ipview" = "see ip public"
     }
 }
-    
-    
 '| Add-Content -Path $Profile.CurrentUserAllHosts -Encoding Default
-
