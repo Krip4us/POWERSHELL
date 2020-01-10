191e41bfff92e11fb0b36b3be2a6da815b514ad9
Krip4us <44360877+Krip4us@users.noreply.github.com>
2020-01-10 12:31:34 +0100

    Update LOGIN.ps1

--- LOGIN.ps1
+++ LOGIN.ps1
@@ -1,45 +1,41 @@
-$usuario = Read-Host "Introduzca usuario: "
-if ($usuario -eq 'hola'){
-Write-Warning "acceso concedido"
-}
+$users = "user1","user2"
+$pass = "pass1","pass2"
 
-else {
-Write-Warning ("Access denied.")
-Write-Warning ("YOU DON'T HAVE PERMISSION.")
-Start-Sleep -Seconds (10)
-cd C:\Users\Admin\acsdenied
-gc .\readme.txt 
-}
+function passwd {
+    $paswdpath = '$.sql.users'
+    $paswd = Read-Host "pass"
 
-if ($usuario -eq 'hola'){
-$pwd = Read-Host "Introduzca password: "
-if ($pwd -eq 'adios'){
-Write-Warning "acceso concedido"
-Write-Host "ACCEDIENDO A LA BASE DE DATOS"
-Start-Sleep -Seconds (3)
-Write-Warning "VERIFICANDO RED SEGURA..."
-cd 'C:\$.setSQL@adm--)'
-Start-Sleep -Seconds (2)
+    if ($pass -contains $paswd) {
+        Test-Path $paswdpath|%{
+            if ($_ -eq 'False') {
+                mkdir $paswdpath
+            }
+            else {
+                cd $paswdpath
+            }
+        }
+    }
+    else {
+        Write-Host "error" -ForegroundColor Red
+    }
+}
+function login {
 
-$kuser = Read-Host "¿QUE USUARIO ERES?: "
-if($kuser -eq 1){
-cd 'C:\$.setSQL@adm--)\1'
+    $read = Read-Host "user"
+    if ($users -contains $read) {
+        
+        Test-Path $read |%{
+            if ($_ -eq "False") {
+                ni -ItemType Directory -Name $read;cd $read
+                passwd
+            }
+            else {
+                passwd
+            }
+        }
+    }
+    else {
+        Write-Host "error" -ForegroundColor Red
+    }
 }
-
-if($kuser -eq 2){
-cd 'C:\$.setSQL@adm--)\2'
-}
-
-if($kuser -eq 3){
-cd 'C:\$.setSQL@adm--)\3'
-}
-}
-}
-else {
-Write-Warning ("Access denied.")
-Write-Warning ("YOU DON'T HAVE PERMISSION.")
-Start-Sleep -Seconds (10)
-cd C:\Users\Admin\acsdenied
-gc .\readme.txt 
-
-}
+login
