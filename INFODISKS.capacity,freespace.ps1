7f5ea536881c146c2dba240361a222f586ed0366
Krip4us <44360877+Krip4us@users.noreply.github.com>
2020-01-10 12:29:50 +0100

    Update and rename get information of disks.ps1 to INFODISKS.capacity,freespace.ps1

--- INFODISKS.capacity,freespace.ps1
+++ INFODISKS.capacity,freespace.ps1
@@ -1,5 +1,1 @@
-$my_disk = %{
 get-wmiobject win32_volume -computername . | select name, @{expression={$_.capacity/1GB}}, @{expression={$_.freespace/1GB}}, @{name=”% Free”;expression={$_.freespace/$_.capacity*100}}
-}
-$my_disk
-$my_disk | Out-File resltados.txt -Append
