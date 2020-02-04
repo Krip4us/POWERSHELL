8523e067068bfca901f749a43d3032dade69d315
Krip4us <44360877+Krip4us@users.noreply.github.com>
2020-02-04 13:24:04 +0100

    Update Interactive Console.ps1

--- Interactive Console.ps1
+++ Interactive Console.ps1
@@ -6,6 +6,7 @@ else {
     New-Item -Path $Profile.CurrentUserAllHosts -ItemType File -Force
 }
 '
+
 Clear-Host;Set-Location $env:USERPROFILE
 function date{
     (Get-Date).ToString("dd.MM.yyyy/HH:mm:ss"); 
@@ -179,7 +180,8 @@ function check{
             "-------------------------------------------"
             ipconfig.exe /displaydns | Out-File displaydns.sys -Verbose
             "-------------------------------------------"
-            Get-NetAdapterBinding -Name "USB-ETH1','Ethernet 2" |Sort-Object -Property Enabled -CaseSensitive | Out-File Get-NetAdapterBinding.sys -Verbose
+            Get-NetAdapterBinding -Name "USB-ETH1
+Ethernet 2" |Sort-Object -Property Enabled -CaseSensitive | Out-File Get-NetAdapterBinding.sys -Verbose
             "-------------------------------------------"
             Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\USBSTOR\*\*\" | Where-Object { $_.FriendlyName } | Select-Object FriendlyName | Out-File USBSTOR.sys -Verbose
             "-------------------------------------------"
@@ -316,13 +318,17 @@ function ipview{
        }
     }
     }
-    
+
     while (1) {
     
         command
     
     }
 }
+function updateconsole{
+    iex (new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Krip4us/POWERSHELL/master/Interactive%20Console.ps1") -Verbose
+}
+getupdateconsole -version
 
         function commands{
             [PSCustomObject]@{
@@ -333,6 +339,7 @@ function ipview{
                 "check" = "check you system"
                 "ipview" = "see ip public"
                 "viewcheck" = "view the last checker file compress"
+                "updateconsole" = "update interactive console"
             }
         }
 '| Add-Content -Path $Profile.CurrentUserAllHosts -Encoding Default
