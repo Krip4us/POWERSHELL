e9f2ab4e4b0f5b5a0695a38a8f2fe1ec8f702626
Krip4us <44360877+Krip4us@users.noreply.github.com>
2020-04-12 12:45:40 +0200

    Update actualizaciones del sistema.ps1

--- actualizaciones del sistema.ps1
+++ actualizaciones del sistema.ps1
@@ -1,5 +1,3 @@
-```powershell
 #Ver las actualizaciones instaladas en el sistema
 Get-HotFix | Select-Object HotFixID
 Get-Package | Select-Object Name | Where-Object Name -match "Actualización" | Format-Custom
-```
