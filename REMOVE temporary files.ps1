551944b15e7e16847e9a2d081c235049f8978806
SPARTAN <44360877+Krip4us@users.noreply.github.com>
2020-04-23 16:12:39 +0200

    Create REMOVE temporary files.ps1

---
+++ REMOVE temporary files.ps1
@@ -0,0 +1,5 @@
+Start-Process powershell{
+(ls $env:TEMP | Select-Object FullName) | ForEach-Object{
+  rm $_.FullName -Recurse -Force <# OPCIONAL: -Confirm:$false -Verbose:$true #>}
+}-verb runas
+<# FIN DEL SCRIPT: REMOVE TEMP FILES  #>
